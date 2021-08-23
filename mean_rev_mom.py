d41d09207f70676587591d564ba2270d8e2cd120
samueljuanp <66521149+samueljuanp@users.noreply.github.com>
2021-08-23 20:08:14 +0800

    Update mean_rev_mom.py

--- mean_rev_mom.py
+++ mean_rev_mom.py
@@ -7,7 +7,7 @@ Created on Sat Aug 14 21:57:48 2021
 
 # import required libraries
 import pandas as pd
-pd.options.display.float_format = "{:,.3f}".format
+pd.options.display.float_format = "{:,.2f}".format
 import pandas_datareader as wb
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,38 +15,57 @@ import seaborn as sns
 sns.set_style('darkgrid')
 
 
+#ticker='SPY'
+#start='2000-01-01'
+#end=None
+#boll=20
+#short=10
+#medium=50
+#long=150
+#capital=10000
+#stop_loss=0.02
+#take_profit=0.03
+#time_loss=40
+#tc=1
+#long_short=True
+#verbose=True
+
+
 # define backtest engine function
-def backtest_strategy (ticker, start='2000-01-01', end='2014-12-31', boll=20, short=20, medium=40, long=100,
-                       stop_loss=0.02, take_profit=0.04, time_loss=40, tc=2, lag=0, long_short=False, verbose=False):
+def backtest_strategy (ticker='SPY', start='2000-01-01', end='2014-12-31',
+                       boll=20, short=10, medium=50, long=100, capital=10000,
+                       stop_loss=0.02, take_profit=0.04, time_loss=40, tc=1,
+                       long_short=True, verbose=False, plot=True, return_pnl=False):
 
     # retrieve price data from Yahoo Finance
     data = wb.DataReader(ticker, 'yahoo', start, end)
 
     # construct bollinger bands to idenfity mean reversion
-    upper_band = data['Adj Close'].rolling(boll).mean() + 2*data['Adj Close'].rolling(boll).std()
-    lower_band = data['Adj Close'].rolling(boll).mean() - 2*data['Adj Close'].rolling(boll).std()
+    upper_band = data['Close'].rolling(boll).mean() + 2*data['Close'].rolling(boll).std()
+    lower_band = data['Close'].rolling(boll).mean() - 2*data['Close'].rolling(boll).std()
 
     # compute moving averages to determine trend or momentum
-    short_ma = data['Adj Close'].rolling(short).mean()
-    medium_ma = data['Adj Close'].rolling(medium).mean()
-    long_ma = data['Adj Close'].rolling(long).mean()
+    short_trend = data['Close'].rolling(short).mean()
+    medium_trend = data['Close'].rolling(medium).mean()
+    long_trend = data['Close'].rolling(long).mean()
 
     # determine long conditions
-    uptrend_mom = (short_ma > medium_ma) | (long_ma.diff() > 0)
-    uptrend_dip = data['Adj Close'] < lower_band
+    uptrend_mom = (short_trend > medium_trend) & (long_trend.diff() > 0)
+    uptrend_dip = data['Close'] < lower_band
     long_trigger = uptrend_mom & uptrend_dip
 
     # determine short conditions
     if long_short:
-        downtrend_mom = (short_ma < medium_ma) | (long_ma.diff() < 0)
-        downtrend_spike = data['Adj Close'] > upper_band
+        downtrend_mom = (short_trend < medium_trend) & (long_trend.diff() < 0)
+        downtrend_spike = data['Close'] > upper_band
         short_trigger = downtrend_mom & downtrend_spike
 
     # prepare signal dataframe
-    signal = long_trigger.copy()
-    signal.loc[:] = 0
+    pnl = long_trigger.copy()
+    pnl.loc[:] = 0
+    pnl.iloc[0] = capital
 
-    # initialize simulation
+    # initialize backtest parameters
     long_trade = False
     short_trade = False
     stop_loss_counter = 0
@@ -54,145 +73,194 @@ def backtest_strategy (ticker, start='2000-01-01', end='2014-12-31', boll=20, sh
     time_loss_counter = 0
 
     # loop over daily ticks
-    for curr_date in signal.index:
+    for i in range(1, len(pnl)):
+
+        # set dates for ease of reference
+        prev_date = pnl.index[i-1]
+        curr_date = pnl.index[i]
+
 
         # do not enter long and short at the same time
         if short_trade is False:
 
             # not trading and no long trigger
-            if (long_trade == False) and (long_trigger.loc[curr_date] == False):
-                pass
+            if (long_trade == False) and (long_trigger.loc[prev_date] == False):
+                pnl.loc[curr_date] = pnl.loc[prev_date]
+
 
             # not trading and got long trigger
-            elif (long_trade == False) and (long_trigger.loc[curr_date] == True):
-                long_date = curr_date
-                long_spot = data.loc[curr_date,'Adj Close']
-                signal.loc[curr_date] = 1
-                long_trade = True
-                if verbose: print(f"Long entry on {str(long_date.date())} at {long_spot:.2f}")
+            elif (long_trade == False) and (long_trigger.loc[prev_date] == True):
+                if data.loc[curr_date,'Open'] > data.loc[prev_date,'Close']:
+                    # update status
+                    long_date = curr_date
+                    long_enter = data.loc[curr_date,'Open']
+                    long_sl = data.loc[prev_date,'Low']
+                    distance = long_enter - long_sl
+                    long_tp = long_enter + (take_profit / stop_loss) * distance
+                    capital = pnl.loc[prev_date]
+                    amount = np.min((stop_loss*capital)/distance, capital/long_enter)
+                    long_trade = True
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (data.loc[curr_date,'Close'] - long_enter)) - tc
+                    if verbose:
+                        print(f"[LONG] Buy {amount:.2f} shares at {long_enter:.2f} on {str(long_date.date())}")
+                        print(f"[LONG] Stop loss: {long_sl:.2f} and take profit: {long_tp:.2f}")
+                else:
+                    pnl.loc[curr_date] = pnl.loc[prev_date]
+
 
             # trading and got long trigger
-            elif (long_trade == True) and (long_trigger.loc[curr_date] == True):
-                # adjust trailing stop loss
-                if data.loc[curr_date,'Adj Close'] > long_spot:
-                    long_spot = data.loc[curr_date,'Adj Close']
-                    if verbose: print(f"Adjust long trailing stop loss to {data.loc[curr_date,'Adj Close']:.2f}")
-                signal.loc[curr_date] = 1
+            #elif (long_trade == True) and (long_trigger.loc[prev_date] == True):
+                # update capital
+                #pnl.loc[curr_date] = capital + (amount * (data.loc[curr_date,'Close'] - long_enter))
+
 
             # trading and no long trigger
-            elif (long_trade == True) and (long_trigger.loc[curr_date] == False):
+            elif (long_trade == True): #and (long_trigger.loc[prev_date] == False):
 
                 # capture current price for ease for reference
-                curr_long_price = data.loc[curr_date,'Adj Close']
+                curr_long_low_price = data.loc[curr_date,'Low']
+                curr_long_high_price = data.loc[curr_date,'High']
 
                 # hit stop loss
-                if curr_long_price < (long_spot * (1-stop_loss)):
-                    signal.loc[curr_date] = 0
+                if curr_long_low_price <= long_sl:
+                    # update status
                     long_trade = False
                     stop_loss_counter += 1
-                    if verbose: print(f"Long exit on {str(curr_date.date())} at {curr_long_price:.2f} due to stop loss\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (long_sl - long_enter)) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose:
+                        print(f"[LONG] Sell {amount:.2f} shares at {long_sl:.2f} on {str(curr_date.date())} due to stop loss")
+                        print(f"[LONG] Loss: {(amount * (long_enter - long_sl)):.2f}\n")
 
                 # hit take profit
-                elif curr_long_price > (long_spot * (1+take_profit)):
-                    signal.loc[curr_date] = 0
+                elif curr_long_high_price >= long_tp:
+                    # update status
                     long_trade = False
                     take_profit_counter += 1
-                    if verbose: print(f"Long exit on {str(curr_date.date())} at {curr_long_price:.2f} due to take profit\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (long_tp - long_enter)) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose:
+                        print(f"[LONG] Sell {amount:.2f} shares at {long_tp:.2f} on {str(curr_date.date())} due to take profit")
+                        print(f"[LONG] Profit: {(amount * (long_tp - long_enter)):.2f}\n")
 
                 # hit time loss
                 elif np.busday_count(long_date.date(), curr_date.date()) > time_loss:
-                    signal.loc[curr_date] = 0
+                    # update status
                     long_trade = False
                     time_loss_counter += 1
-                    if verbose: print(f"Long exit on {str(curr_date.date())} at {curr_long_price:.2f} due to time loss\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (data.loc[curr_date,'Open'] - long_enter)) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose:
+                        print(f"[LONG] Sell {amount:.2f} shares at {data.loc[curr_date,'Open']:.2f} on {str(curr_date.date())} due to time loss")
+                        print(f"[LONG] P/L: {(amount * (data.loc[curr_date,'Open'] - long_enter)):.2f}\n")
 
+                # continue trade
                 else:
-                    signal.loc[curr_date] = 1 # continue trade
+                    pnl.loc[curr_date] = capital + (amount * (data.loc[curr_date,'Close'] - long_enter))
 
-        ########################## HANDLE LONG/SHORT CASE ##########################
+
+        ############################# HANDLE LONG/SHORT CASE #############################
+
 
         if long_short and (long_trade is False):
 
             # not trading and no short trigger
-            if (short_trade == False) and (short_trigger.loc[curr_date] == False):
-                pass
+            if (short_trade == False) and (short_trigger.loc[prev_date] == False):
+                pnl.loc[curr_date] = pnl.loc[prev_date]
+
 
             # not trading and got short trigger
-            elif (short_trade == False) and (short_trigger.loc[curr_date] == True):
+            elif (short_trade == False) and (short_trigger.loc[prev_date] == True):
+                # update status
                 short_date = curr_date
-                short_spot = data.loc[curr_date,'Adj Close']
-                signal.loc[curr_date] = -1
+                short_enter = data.loc[curr_date,'Open']
+                short_sl = data.loc[prev_date,'High']
+                short_tp = short_enter - (take_profit / stop_loss) * (short_sl - short_enter)
+                capital = pnl.loc[prev_date]
+                amount = (stop_loss * capital) / (short_sl - short_enter)
                 short_trade = True
-                if verbose: print(f"Short entry on {str(short_date.date())} at {short_spot:.2f}")
+                # update capital
+                pnl.loc[curr_date] = capital + (amount * (short_enter - data.loc[curr_date,'Close'])) - tc
+                if verbose: print(f"[SHORT] Sell {amount:.2f} shares at {short_enter:.2f} on {str(short_date.date())}")
+
 
             # trading and got short trigger
-            elif (short_trade == True) and (short_trigger.loc[curr_date] == True):
-                # adjust trailing stop loss
-                if data.loc[curr_date,'Adj Close'] < short_spot:
-                    short_spot = data.loc[curr_date,'Adj Close']
-                    if verbose: print(f"Adjust short trailing stop loss to {data.loc[curr_date,'Adj Close']:.2f}")
-                signal.loc[curr_date] = -1
+            elif (short_trade == True) and (short_trigger.loc[prev_date] == True):
+                # update capital
+                pnl.loc[curr_date] = capital + (amount * (short_enter - data.loc[curr_date,'Close']))
+
 
             # trading and no short trigger
-            elif (short_trade == True) and (short_trigger.loc[curr_date] == False):
+            elif (short_trade == True) and (short_trigger.loc[prev_date] == False):
 
                 # capture current price for ease for reference
-                curr_short_price = data.loc[curr_date,'Adj Close']
+                curr_short_low_price = data.loc[curr_date,'Low']
+                curr_short_high_price = data.loc[curr_date,'High']
 
                 # hit stop loss
-                if curr_short_price > (short_spot * (1+stop_loss)):
-                    signal.loc[curr_date] = 0
-                    short_trade = False
+                if curr_short_high_price >= short_sl:
+                    # update status
+                    short_trade=False
                     stop_loss_counter += 1
-                    if verbose: print(f"Short exit on {str(curr_date.date())} at {curr_short_price:.2f} due to stop loss\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (short_enter - short_sl)) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose: print(f"[SHORT] Buy {amount:.2f} shares at {short_sl:.2f} on {str(curr_date.date())} due to stop loss\n")
 
                 # hit take profit
-                elif curr_short_price < (short_spot * (1-take_profit)):
-                    signal.loc[curr_date] = 0
+                elif curr_short_low_price <= short_tp:
+                    # update status
                     short_trade = False
                     take_profit_counter += 1
-                    if verbose: print(f"Short exit on {str(curr_date.date())} at {curr_short_price:.2f} due to take profit\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (short_enter - short_tp)) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose: print(f"[SHORT] Buy {amount:.2f} shares at {short_tp:.2f} on {str(curr_date.date())} due to take profit\n")
 
                 # hit time loss
                 elif np.busday_count(short_date.date(), curr_date.date()) > time_loss:
-                    signal.loc[curr_date] = 0
+                    # update status
                     short_trade = False
                     time_loss_counter += 1
-                    if verbose: print(f"Short exit on {str(curr_date.date())} at {curr_short_price:.2f} due to time loss\n")
+                    # update capital
+                    pnl.loc[curr_date] = capital + (amount * (short_enter - data.loc[curr_date,'Open'])) - tc
+                    capital = pnl.loc[curr_date]
+                    if verbose: print(f"[SHORT] Buy {amount:.2f} shares at {data.loc[curr_date,'Open']:.2f} on {str(curr_date.date())} due to time loss\n")
 
+                # continue trade
                 else:
-                    signal.loc[curr_date] = -1 # continue trade
+                    pnl.loc[curr_date] = capital + (amount * (short_enter - data.loc[curr_date,'Close']))
 
 
-    # compute strategy return with specified execution lag
-    daily_return = data['Adj Close'].pct_change() * signal.shift(1+lag)
-
-    # include transaction cost
-    cost = tc / 10000
-    turnover = signal.shift(lag).diff().fillna(0).abs()
-    daily_return = daily_return - (turnover * cost)
+    # compute daily return
+    daily_return = pnl.diff()
 
     # compute annualized sharpe ratio
-    sharpe = daily_return.mean() / daily_return.std() * np.sqrt(252)
+    average_gain = daily_return[daily_return > 0].mean()
+    average_loss = daily_return[daily_return < 0].abs().mean()
+    edge = average_gain / average_loss
+
+    # compute take profit / stop loss ratio
+    tp_sl_ratio = take_profit_counter / stop_loss_counter
 
     # compute maximum drawdown
-    cumulative_pnl = daily_return.cumsum(skipna=True)
-    high_watermark = cumulative_pnl.cummax()
-    daily_drawdown = cumulative_pnl - high_watermark
-    max_drawdown = daily_drawdown.abs().max() * 100
-
-    # compute annualized return
-    annual_return = np.prod(daily_return.resample('A').sum() + 1) \
-                    ** (1/len(daily_return.resample('A').sum())) - 1
-    annual_return = annual_return * 100
+    high_watermark = pnl.cummax()
+    daily_drawdown = pnl - high_watermark
+    max_drawdown = (daily_drawdown / high_watermark).abs().max() * 100
 
     # visualize performance
-    plt.figure(figsize=(16,9))
-    plt.title(f"Strategy Performance of {ticker}", fontsize=13)
-    plt.ylabel('Cumulative PnL (%)', fontsize=12)
-    plt.plot(cumulative_pnl * 100,
-             label=f"Annualized Return = {annual_return:.2f}% | Sharpe = {sharpe:.2f} | Max.DD = {max_drawdown:.2f}%")
-    plt.legend(loc='upper left', fontsize=12)
-    plt.show()
+    if plot:
+        plt.figure(figsize=(16,9))
+        plt.title(f"Strategy Performance for {ticker}", fontsize=13)
+        plt.ylabel('Cumulative PnL', fontsize=12)
+        plt.plot(pnl, label=f"Trading Edge = {edge:.2f} | TP/SL Ratio = {tp_sl_ratio:.2f} | Max.DD = {max_drawdown:.2f}%")
+        plt.legend(loc='upper left', fontsize=12)
+        plt.show()
 
-    return print(f"\nStop Loss: {stop_loss_counter}\nTake Profit: {take_profit_counter}\nTime Loss: {time_loss_counter}")
+    # return pnl
+    if return_pnl:
+        return pnl
